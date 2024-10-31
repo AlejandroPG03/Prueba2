d5283604a87d0577bd42ac2f5a755026df085fc4
soyduri <149092601+soyduri@users.noreply.github.com>
2024-10-31 18:27:14 +0100

    he puesto mi nombre

--- EnunciadoJaviClaseMatrices/src/Main/MainClass.java
+++ EnunciadoJaviClaseMatrices/src/Main/MainClass.java
@@ -2,35 +2,36 @@ package Main;
 
 public class MainClass {
 	public static final int N = 3;
-	
+
 	public static void main(String[] args) {
 		int[][] matrizBizarra = new int[N][N];
 		cargaMatriz(matrizBizarra);
 		muestraMatriz(matrizBizarra);
 		muestraSilla(matrizBizarra);
-		
+
 	}
-	
+
 	public static void cargaMatriz(int[][] array) {
-		for (int i = 0 ; i < array.length ; i ++ ) {
-			for(int j = 0 ; j < array.length ; j ++) {
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length; j++) {
 				array[i][j] = (int) (Math.random() * 90 + 10);
-				//Carlos estuvo aqui.
+				// Carlos Puta estuvo aqui.
 				// Dani estuvo aqui
+				// y Duri
 			}
 		}
 	}
-	
+
 	public static void muestraMatriz(int[][] array) {
 
-		for (int i = 0 ; i < array.length ; i ++ ) {
-			for(int j = 0 ; j < array.length ; j ++) {
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length; j++) {
 				System.out.print(array[i][j] + " ");
 			}
 			System.out.println();
 		}
 	}
-	
+
 	public static void muestraSilla(int[][] array) {
 		for (int i = 0 ; i < array.length ; i ++ ) {
 			for(int j = 1 ; j < array.length ; j ++) {
